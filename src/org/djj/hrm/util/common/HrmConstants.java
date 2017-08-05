329a210c42432adc94a0ec5ef3f9ad618ffe371e
Demo-du <1556141770@qq.com>
2017-08-05 10:22:13 +0800

    常量

---
+++ src/org/djj/hrm/util/common/HrmConstants.java
@@ -0,0 +1,17 @@
+package org.djj.hrm.util.common;
+
+public class HrmConstants {
+    //数据表常量
+	public static final String USERTABLE="user_inf";
+	public static final String DEPTTABLE="dept_inf";
+	public static final String JOBTABLE="job_inf";
+	public static final String EMPLOYEETABLE="employee_inf";
+	public static final String NOTICETABLE="notice_inf";
+	public static final String DOCUMENTTABLE="document_inf";
+	//===========================================================//
+	public static final String LOGIN="loginForm";
+	//session
+	public static final String USERSESSION="user_session";
+	//每页数据
+	public static int PAGE_DEFAULT_SIZE=4;
+}
